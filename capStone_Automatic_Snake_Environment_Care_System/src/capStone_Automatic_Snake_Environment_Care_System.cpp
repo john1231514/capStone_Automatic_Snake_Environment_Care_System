fa0684b9a8883d76b2c3709b8ae4f8b3f9dbc790
jonhtanan richrs <jisr1012@gmail.com>
2024-08-12 11:44:48 -0600

    added comments and date name and Description and project name

--- capStone_Automatic_Snake_Environment_Care_System/src/capStone_Automatic_Snake_Environment_Care_System.cpp
+++ capStone_Automatic_Snake_Environment_Care_System/src/capStone_Automatic_Snake_Environment_Care_System.cpp
@@ -1,7 +1,8 @@
 /* 
- * Project myProject
- * Author: Your Name
- * Date: 
+ * Project capStone_Automatic_Snake_Environment_Care_System
+ * Description: This my capstone for the class, it's a system to take care of my snakes environment when i am not available.
+ * Author: Johnathan
+ * Date: 8/12/2024
  * For comprehensive documentation and examples, please visit:
  * https://docs.particle.io/firmware/best-practices/firmware-template/
  */
@@ -9,10 +10,11 @@
 // Include Particle Device OS APIs
 #include "Particle.h"
 
+//this is the header, it defines variables which are used in the code.
 #define Grove_Water_Sensor D7
 int water;
 int currentTime;
-int last150millisSec;
+int last150MillisSec;
 
 // Let Device OS manage the connection to the Particle Cloud
 SYSTEM_MODE(AUTOMATIC);
@@ -26,8 +28,8 @@ void setup() {
 void loop() {
    water=digitalRead(Grove_Water_Sensor);
  currentTime = millis ();
-  if((currentTime-last150millisSec)>150) {
-    last150millisSec = millis();
+  if((currentTime-last150MillisSec)>150) {
+    last150MillisSec = millis();
  Serial.printf("water %i ", water);
 }
 }
